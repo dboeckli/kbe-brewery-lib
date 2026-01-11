727cb9b797d7655d048887e2e966dc1c7da3a7f1
Dominique Boeckli <dboeckli@gmail.com>
2026-01-11 19:04:46 +0100

    fix upgrade jackson

--- src/main/java/ch/guru/springframework/kbe/lib/dto/BeerPagedList.java
+++ src/main/java/ch/guru/springframework/kbe/lib/dto/BeerPagedList.java
@@ -19,10 +19,10 @@ package ch.guru.springframework.kbe.lib.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import tools.jackson.databind.JsonNode;
 
 import java.util.List;
 

92f29f578b7175d31160b52360cb9bc919b3926e
Dominique Boeckli <dboeckli@gmail.com>
2026-08-29 10:58:17 +0200

    fix: serialize dates RFC 3339 compliant (Z -> XXX) (#70)

--- src/main/java/ch/guru/springframework/kbe/lib/dto/BeerOrderDto.java
+++ src/main/java/ch/guru/springframework/kbe/lib/dto/BeerOrderDto.java
@@ -39,11 +39,11 @@ public class BeerOrderDto {
 
     private Integer version = null;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", shape = JsonFormat.Shape.STRING)
     @JsonProperty("createdDate")
     private OffsetDateTime createdDate = null;
 
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssXXX", shape = JsonFormat.Shape.STRING)
     @JsonProperty("lastModifiedDate")
     private OffsetDateTime lastModifiedDate = null;
 
